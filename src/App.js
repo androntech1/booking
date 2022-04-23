c7bfc450173d6953ac5c9842228c06a791add760
androntech1 <androntech1@gmail.com>
2022-04-23 19:22:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Button, DatePicker, Checkbox, Card, Avatar, List } from 'antd';
+import moment from 'moment';
+import { TIME } from './data';
+import 'antd/dist/antd.css';
 
-function App() {
+export default function App() {
+  const [time, setTime] = useState(TIME);
+  const [cards, setCards] = useState(false);
+  const [isCardVisible, setIsCardVisible] = useState(false);
+  const [date, setDate] = useState();
+
+  const { Meta } = Card;
+  const { RangePicker } = DatePicker;
+  const dateFormat = 'DD-MM-YYYY';
+
+  const onChange = (date, dateString) => setDate(date);
+  const onButtonClick = () => setCards(true);
+
+  //----------------------------------------------------------
+  const minDate = moment().format(dateFormat);
+  const maxDate = moment().add(1, 'months').format(dateFormat);
+
+  const disabledDate = (current) => {
+    let customDate = moment().format(dateFormat);
+    return current && current < moment(customDate, dateFormat);
+  };
+
+  const sortAvailable = (e) =>
+    e.target.checked
+      ? setTime(TIME.filter(({ available }) => available))
+      : setTime(TIME);
+
+  const getText = (isAvailable) =>
+    isAvailable ? (
+      <Checkbox onChange={onChange}>Book slot</Checkbox>
+    ) : (
+      'Already Booked 🛑'
+    );
+
+  const onOpenChange = (open) => console.log(open);
+
+  useEffect(() => {
+    date && cards && setIsCardVisible(true);
+  }, [date, cards]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ flex: 1, margin: 20 }}>
+      <h1 style={{ alignSelf: 'center' }}>Hello Player 👋</h1>
+      <DatePicker
+        disabledDate={disabledDate}
+        onChange={onChange}
+        onOpenChange={onOpenChange}
+      />
+      {/* <RangePicker disabledDate={disabledDate} /> */}
+      <Button onClick={onButtonClick} type="primary" style={{ marginLeft: 8 }}>
+        Search
+      </Button>
+      {isCardVisible && (
+        <Checkbox style={{ marginLeft: 20 }} onChange={sortAvailable}>
+          Show only available slots
+        </Checkbox>
+      )}
+      {isCardVisible && (
+        <>
+        <List
+          style={{ marginTop: 30 }}
+          grid={{
+            gutter: 16,
+            xs: '1',
+            sm: '2',
+            md: '2',
+            lg: '2',
+            xl: '4',
+            xxl: '4',
+          }}
+          dataSource={time}
+          renderItem={({ slot, available }) => (
+            <List.Item>
+              <Card
+                style={{
+                  width: 300,
+                  marginTop: 16,
+                  backgroundColor: 'light-pink',
+                }}
+                loading={false}
+              >
+                <Meta
+                  avatar={'🏸'}
+                  title={`Time slot ${slot}`}
+                  description={getText(available)}
+                />
+              </Card>
+            </List.Item>
+          )}
+        />
+        <Button onClick={()=>alert('Ok')} type="primary" style={{ marginLeft: 8 }}>
+        Book now
+      </Button></>
+      )}
     </div>
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,70 @@
+export const TIME = [
+  {
+    slot: '5 to 6',
+    available: true,
+  },
+  {
+    slot: '6 to 7',
+    available: true,
+  },
+  {
+    slot: '7 to 8',
+    available: false,
+  },
+  {
+    slot: '8 to 9',
+    available: false,
+  },
+  {
+    slot: '9 to 10',
+    available: false,
+  },
+  {
+    slot: '10 to 11',
+    available: false,
+  },
+  {
+    slot: '11 to 12',
+    available: true,
+  },
+  {
+    slot: '12 to 13',
+    available: true,
+  },
+  {
+    slot: '13 to 14',
+    available: true,
+  },
+  {
+    slot: '14 to 15',
+    available: true,
+  },
+  {
+    slot: '15 to 16',
+    available: false,
+  },
+  {
+    slot: '16 to 17',
+    available: false,
+  },
+  {
+    slot: '17 to 18',
+    available: true,
+  },
+  {
+    slot: '18 to 19',
+    available: true,
+  },
+  {
+    slot: '19 to 20',
+    available: true,
+  },
+  {
+    slot: '20 to 21',
+    available: true,
+  },
+  {
+    slot: '21 to 22',
+    available: true,
+  },
+];
